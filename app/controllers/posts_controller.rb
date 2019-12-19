32f247a8098b0c8387fbe23748a33a867e120b6f
sugawara <suga0815_0321@yahoo.co.jp>
2019-12-19 12:52:26 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+  before_action :set_target_post, only: %i[show edit update destroy]
+  
+  
+  def index
+    @posts = Post.page(params[:page])
+  end
+  
+  def new
+     @post = Post.new
+  end 
+ 
+  
+  def create
+    post = Post.create(post_params)
+    redirect_to post 
+  end 
+  
+  def show
+  end 
+  
+  def edit
+  end
+  
+  def update
+    @post.update(post_params)
+    
+    redirect_to @post
+  end
+  
+  def destroy
+    @post.delete
+    
+    redirect_to posts_path
+  end 
+  
+  private
+  
+  def post_params
+     params.require(:post).permit(:name,:title,:body)
+  end 
+  
+  def set_target_post
+    @post = Post.find(params[:id])
+  end
+end
