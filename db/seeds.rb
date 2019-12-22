1a784fbaa3e39fcabb50e03928a3c841e606d163
sugawara <suga0815_0321@yahoo.co.jp>
2019-12-22 14:20:30 +0000

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ Tag.create([
+   
+   { name: "Ruby" },
+   { name: "Ruby on Rails" },
+   { name: "python"},
+   { name: "Django"},
+   { name: "php"},
+   { name: "Laravel" }
+     
+   ])
+ 
