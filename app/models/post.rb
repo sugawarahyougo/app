f3ac9c919c719a5f97eef41f1ad075b9bd59662f
sugawara <suga0815_0321@yahoo.co.jp>
2019-12-22 07:39:24 +0000

    first commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,8 @@
 
 class Post < ApplicationRecord
   has_many :comments
+  has_many :post_tag_relations
+  has_many :tags, through: :post_tag_relations
   
   validates :name,presence: true, length: {maximum: 10}
   validates :title,presence: true, length: {maximum: 30}
