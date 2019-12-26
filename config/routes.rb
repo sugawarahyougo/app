653090f09daf88f2da8b38e156753573c8f26f56
sugawara <suga0815_0321@yahoo.co.jp>
2019-12-26 02:19:32 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root 'posts#index'
+  get "mypage" => "users#me"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
+  
+ 
+  root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   resources :comments, only: %i[create destroy]
+  resources :users, only: %i[new create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_024949) do
+ActiveRecord::Schema.define(version: 2019_12_25_020112) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2019_12_22_024949) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
